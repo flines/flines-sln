99b16ec992b85536d47b218b5a448d5977773836
flinesguo <flines@126.com>
2018-09-18 21:52:03 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import View from '/src/components/View';
-import('./components/View').then((View) => {
+import('./components/View.js').then((View) => {
     document.getElementById('app').appendChild(document.createElement('div', {innerHTML: View.default}));
 });
 
